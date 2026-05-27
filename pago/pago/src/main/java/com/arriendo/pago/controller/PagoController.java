1bedf1f0568f2a0f256c8f8f509b578357ab11ac
seban <s.navarrete@duocuc.cl>
2026-05-26 20:35:01 -0400

    feat: pago - agregar validacion de roles CLIENTE

--- pago/pago/src/main/java/com/arriendo/pago/controller/PagoController.java
+++ pago/pago/src/main/java/com/arriendo/pago/controller/PagoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -23,12 +24,15 @@ public class PagoController {
     private PagoService service;
 
     @PostMapping("/procesar")
-    public ResponseEntity<String> procesarPago(@Valid @RequestBody PagoModel model) {
+    public ResponseEntity<?> procesarPago(@Valid @RequestBody PagoModel model, @RequestParam String rol) {
+        if (!rol.equals("CLIENTE")) {
+            return ResponseEntity.status(403).body("Solo un CLIENTE puede procesar pagos");
+        }
         return ResponseEntity.ok(service.procesarPago(model));
     }
-@GetMapping
-public ResponseEntity<List<pago>> obtenerTodo(){
-    return ResponseEntity.ok(service.obtenerTodo());
-}
 
+    @GetMapping
+    public ResponseEntity<List<pago>> obtenerTodo(){
+        return ResponseEntity.ok(service.obtenerTodo());
+    }
 }
